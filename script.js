175c21e5cfb9e84b8c4f1e0ec15c4757c9b62c80
jswelsh <james.sinclair.welsh@gmail.com>
2020-02-18 18:42:11 -0800

    changed to a if else, easier to read but longer

--- script.js
+++ script.js
@@ -30,6 +30,23 @@ for (let i = 0; i < 100;)
     console.log( 
         (++i % 3 ? '':'Fizz') + (i % 5 ? '':'Buzz') || i
         )  */
-
+/* 
+//wont work in sctrict
 console.log("type 5") 
-    for (i = 0; i < 100;) console.log( (++i % 3 ? '':'Fizz') + (i % 5 ? '':'Buzz') || i)
+    for (i = 0; i < 100;) console.log( (++i % 3 ? '':'Fizz') + (i % 5 ? '':'Buzz') || i) 
+*/
+
+console.log("type 6");
+let i = 0;
+while (i < 100) {
+    i = i + 1;
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    }else if (i % 3 === 0){
+        console.log('Fizz');
+    }else  if(i % 5 === 0){
+        console.log('Buzz');
+    } else console.log(i.toString());
+}
+
